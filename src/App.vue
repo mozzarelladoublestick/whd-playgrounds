0c6e11ded8677e7c74c63cd7e477fc0bc5f63279
mozzarelladoublestick <92583850+mozzarelladoublestick@users.noreply.github.com>
2025-07-21 10:54:30 +0200

    chore: initial project setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <teleport to="body">
+    <main id="content">
+      <wm-stage>
+        <h1 slot="content">Spielplätze in Wien entdecken</h1>
+      </wm-stage>
+    </main>
+  </teleport>
+  <Footer></Footer>
+</template>
+<style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <teleport to="body">
+    <footer>
+      <wm-copyright></wm-copyright>
+      <wm-list type="horizontal" alignment="center" separator="pipe">
+        <ul>
+          <li>
+            <a href="#"><strong>Impressum</strong></a>
+          </li>
+          <li>
+            <a href="#"><strong>Datenschutz</strong></a>
+          </li>
+          <li>
+            <a href="#"><strong>Barrierefreiheit</strong></a>
+          </li>
+
+          <li>
+            <a href="#"><strong>Öffentliche Verlautbarungen</strong></a>
+          </li>
+          <li>
+            <a href="#"><strong>Medienservice</strong></a>
+          </li>
+        </ul>
+      </wm-list>
+      © Stadt Wien, Rathaus, A-1010 Wien
+    </footer>
+  </teleport>
+</template>
