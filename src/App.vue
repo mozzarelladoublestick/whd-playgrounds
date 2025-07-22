144a72450176f8ece838e621573e06974bfa3524
mozzarelladoublestick <92583850+mozzarelladoublestick@users.noreply.github.com>
2025-07-22 19:21:40 +0200

    feat: add leaflet map

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import { computed, onMounted } from "vue";
 import { ref } from "vue";
+import { nextTick } from "vue";
+import { watch } from "vue";
 
 const districts = [
   { id: 1, name: "Innere Stadt" },
@@ -37,14 +39,79 @@ const getData = async () => {
 };
 const filteredData = computed(() => {
   if (!data.value.features) return [];
+  console.log("changed value to", selectedDistrict.value);
   return data.value.features.filter(
     (playground) => playground.properties.BEZIRK === selectedDistrict.value
   );
 });
-
+watch(filteredData, () => {
+  updateMap();
+});
 onMounted(async () => {
   data.value = await getData();
+  setupMap();
 });
+var map;
+
+let markers = [];
+function setupMap() {
+  console.log("setting up map");
+  map = L.map("map").setView([48.21664832, 16.37190332], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  updateMap();
+}
+
+function updateMap() {
+  markers.forEach((marker) => map.removeLayer(marker));
+  markers.length = 0;
+  var greenIcon = L.icon({
+    iconUrl: "/icons/SW_Piktogramme_Standortzeichen.svg",
+    iconSize: [38, 38], // size of the icon
+
+    className: "white-bg",
+  });
+  filteredData.value.forEach((playground) => {
+    const marker = L.marker(
+      [playground.geometry.coordinates[1], playground.geometry.coordinates[0]],
+      { icon: greenIcon }
+    ).addTo(map);
+    marker.icon;
+    marker.bindTooltip(
+      `
+        <p class="tooltip"><strong>${
+          playground.properties.ANL_NAME
+        }</strong><br />
+        ${playground.properties.SPIELPLATZ_DETAIL.replace(/^,+\s*/, "")}</p>
+      `
+    );
+    markers.push(marker);
+  });
+
+  nextTick(() => {
+    if (markers.length > 0) {
+      const bounds = L.latLngBounds(markers.map((m) => m.getLatLng()));
+      map.fitBounds(bounds, { padding: [50, 50] });
+    }
+  });
+}
+
+function showOnMap(id) {
+  var playground = filteredData.value.filter(
+    (playground) => playground.id === id
+  );
+  console.log(playground[0].geometry.coordinates);
+  map.setView(
+    [
+      playground[0].geometry.coordinates[1],
+      playground[0].geometry.coordinates[0],
+    ],
+    18
+  );
+}
 </script>
 
 <template>
@@ -54,28 +121,77 @@ onMounted(async () => {
       <wm-stage>
         <h1 slot="content">Spielplätze in Wien entdecken</h1>
       </wm-stage>
-      <select label="Bezirk" v-model="selectedDistrict">
-        <option
-          v-for="district in districts"
-          :value="district.id"
-          :key="district.id"
-        >
-          {{ district.id }}., {{ district.name }}
-        </option>
-      </select>
-      <p>Gefundene Spielplätze: {{ filteredData.length }}</p>
-      <wm-list type="row" gap="xs">
-        <ul>
-          <li v-for="playground in filteredData">
-            <div>
-              <strong>{{ playground.properties.ANL_NAME }}</strong>
-              <p>{{ playground.properties.SPIELPLATZ_DETAIL }}</p>
-            </div>
-          </li>
-        </ul></wm-list
-      >
+      <div class="container">
+        <div>
+          <select label="Bezirk" v-model="selectedDistrict">
+            <option
+              v-for="district in districts"
+              :value="district.id"
+              :key="district.id"
+            >
+              {{ district.id }}., {{ district.name }}
+            </option>
+          </select>
+          <p>Gefundene Spielplätze: {{ filteredData.length }}</p>
+          <div class="list">
+            <wm-list type="row" gap="xs">
+              <ul>
+                <li v-for="playground in filteredData" class="cursor-pointer">
+                  <div @click="showOnMap(playground.id)">
+                    <strong>{{ playground.properties.ANL_NAME }}</strong>
+                    <!--use Regex to remove commas and whitespaces of faulty entries-->
+                    <p>
+                      {{
+                        playground.properties.SPIELPLATZ_DETAIL.replace(
+                          /^,+\s*/,
+                          ""
+                        )
+                      }}
+                    </p>
+                  </div>
+                </li>
+              </ul></wm-list
+            >
+          </div>
+        </div>
+        <div id="map"></div>
+      </div>
     </main>
   </teleport>
   <Footer></Footer>
 </template>
-<style scoped></style>
+<style>
+.container {
+  display: flex;
+  gap: 2rem;
+}
+#map {
+  height: 600px;
+  width: 600px;
+}
+.list {
+  max-height: 600px;
+  overflow-y: scroll;
+}
+@font-face {
+  font-family: "WienerMelangeVF";
+  src: url("https://assets.wien.gv.at/theme/202/fonts/WienerMelangeVF.woff2")
+    format("woff2");
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+.leaflet-tooltip {
+  font-family: "WienerMelangeVF", sans-serif;
+  font-size: 14px;
+  color: #333;
+}
+.cursor-pointer {
+  cursor: pointer;
+}
+.white-bg {
+  background-color: white;
+  border-radius: 100%;
+  aspect-ratio: 1/1;
+}
+</style>
